e18d2f09ce0d747be7c30fd13f38376f5780419d
jole <jonas@jole.xyz>
2022-07-26 20:42:55 +0200

    move MpvPlayer to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use async_trait::async_trait;
 use futures::FutureExt;
-use mpvi::{option, Mpv};
 use rumqttc::{AsyncClient, EventLoop, MqttOptions, QoS};
 use serde::{Deserialize, Serialize};
-use std::{
-    process::{Command, Stdio},
-    thread,
-    time::Duration,
-};
+use std::time::Duration;
+
+mod mpv;
+pub use mpv::MpvPlayer;
 
 use tokio::sync::mpsc;
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -34,68 +32,6 @@ pub enum VideoMessage {
 pub trait VideoPlayer {
     fn start(&self) -> std::process::Child;
     async fn run(self, rx: mpsc::Receiver<VideoMessage>);
-}
-
-pub struct MpvPlayer {
-    pub ipc_path: String,
-}
-
-#[async_trait]
-impl VideoPlayer for MpvPlayer {
-    fn start(&self) -> std::process::Child {
-        let ipc_arg = format!("{}={}", "--input-ipc-server", self.ipc_path);
-
-        let c = Command::new("mpv")
-            .arg(ipc_arg)
-            .arg("--idle")
-            .stdin(Stdio::null())
-            .stdout(Stdio::null())
-            // keep stderr
-            .spawn()
-            .unwrap();
-
-        // ew
-        thread::sleep(Duration::from_millis(400));
-
-        c
-    }
-
-    async fn run(self, mut rx: mpsc::Receiver<VideoMessage>) {
-        let mpv = Mpv::new(&self.ipc_path).await.unwrap_or_else(|e| {
-            println!("error connecting to mpv, is mpv running?");
-            println!(
-                r#"start with "mpv --input-ipc-server={} --idle""#,
-                self.ipc_path
-            );
-            panic!("{}", e);
-        });
-
-        loop {
-            let msg = rx.recv().await.expect("closed");
-            println!("mpv: {:?}", &msg);
-            match msg {
-                VideoMessage::Pause => {
-                    mpv.pause().await.expect("failed to pause");
-                }
-                VideoMessage::Unpause => {
-                    mpv.unpause().await.expect("failed to unpause");
-                }
-                VideoMessage::Seek(pos) => {
-                    mpv.seek(pos, option::Seek::Absolute)
-                        .await
-                        .expect("failed to seek");
-                }
-                VideoMessage::Media(path) => {
-                    mpv.load_file(&path, option::Insertion::Replace)
-                        .await
-                        .expect("failed to load file");
-
-                    // start off paused
-                    mpv.pause().await.expect("failed to pause");
-                }
-            }
-        }
-    }
 }
 
 pub struct UniplayOpts {
---
+++ src/mpv.rs
@@ -0,0 +1,72 @@
+use async_trait::async_trait;
+use mpvi::{option, Mpv};
+use std::{
+    process::{Command, Stdio},
+    thread,
+    time::Duration,
+};
+use tokio::sync::mpsc;
+
+use crate::{VideoMessage, VideoPlayer};
+
+pub struct MpvPlayer {
+    pub ipc_path: String,
+}
+
+#[async_trait]
+impl VideoPlayer for MpvPlayer {
+    fn start(&self) -> std::process::Child {
+        let ipc_arg = format!("{}={}", "--input-ipc-server", self.ipc_path);
+
+        let c = Command::new("mpv")
+            .arg(ipc_arg)
+            .arg("--idle")
+            .stdin(Stdio::null())
+            .stdout(Stdio::null())
+            // keep stderr
+            .spawn()
+            .unwrap();
+
+        // ew
+        thread::sleep(Duration::from_millis(400));
+
+        c
+    }
+
+    async fn run(self, mut rx: mpsc::Receiver<VideoMessage>) {
+        let mpv = Mpv::new(&self.ipc_path).await.unwrap_or_else(|e| {
+            println!("error connecting to mpv, is mpv running?");
+            println!(
+                r#"start with "mpv --input-ipc-server={} --idle""#,
+                self.ipc_path
+            );
+            panic!("{}", e);
+        });
+
+        loop {
+            let msg = rx.recv().await.expect("closed");
+            println!("mpv: {:?}", &msg);
+            match msg {
+                VideoMessage::Pause => {
+                    mpv.pause().await.expect("failed to pause");
+                }
+                VideoMessage::Unpause => {
+                    mpv.unpause().await.expect("failed to unpause");
+                }
+                VideoMessage::Seek(pos) => {
+                    mpv.seek(pos, option::Seek::Absolute)
+                        .await
+                        .expect("failed to seek");
+                }
+                VideoMessage::Media(path) => {
+                    mpv.load_file(&path, option::Insertion::Replace)
+                        .await
+                        .expect("failed to load file");
+
+                    // start off paused
+                    mpv.pause().await.expect("failed to pause");
+                }
+            }
+        }
+    }
+}
